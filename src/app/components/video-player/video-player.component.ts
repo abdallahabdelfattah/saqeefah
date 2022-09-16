2cf30b4a9e47264ed3935461801deb08e9f76bdf
@Mohamed.Salem <mohamed.salem@syngenta.com>
2022-09-17 00:03:47 +0200

    update video

--- src/app/components/video-player/video-player.component.ts
+++ src/app/components/video-player/video-player.component.ts
@@ -1,51 +1,68 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-video-player',
   templateUrl: './video-player.component.html',
-  styleUrls: ['./video-player.component.scss']
+  styleUrls: ['./video-player.component.scss'],
 })
-export class VideoPlayerComponent implements OnInit {
-  constructor() { }
+export class VideoPlayerComponent implements OnInit, AfterViewInit {
+  constructor() {}
+  @Input() src: string = 'https://vjs.zencdn.net/v/oceans.mp4';
+  @Input() type: string = 'video/mp4';
+  @Input() loop: string;
 
-  ngOnInit(): void {
-    this.playPause()
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.playPause();
+    }, 1000);
   }
 
-  name = "Angular";
-  @ViewChild("videoPlayer", { static: false }) videoplayer: ElementRef;
+  name = 'Angular';
+  @ViewChild('videoPlayer', { static: false }) videoplayer: ElementRef;
   isPlay: boolean = false;
-  toggleVideo(event: any) {
-    this.videoplayer.nativeElement.play();
-  }
+
   playPause() {
-    var myVideo: any = document.getElementById("my_video_1");
+    var myVideo: any = document.getElementById('my_video_1');
     if (myVideo.paused) myVideo.play();
     else myVideo.pause();
   }
 
+  playLoop() {
+    var myVideo: any = document.getElementById('my_video_1');
+    if (this.loop == 'true') myVideo.loop = true;
+  }
+
   makeBig() {
-    var myVideo: any = document.getElementById("my_video_1");
+    var myVideo: any = document.getElementById('my_video_1');
     myVideo.width = 560;
   }
 
   makeSmall() {
-    var myVideo: any = document.getElementById("my_video_1");
+    var myVideo: any = document.getElementById('my_video_1');
     myVideo.width = 320;
   }
 
   makeNormal() {
-    var myVideo: any = document.getElementById("my_video_1");
+    var myVideo: any = document.getElementById('my_video_1');
     myVideo.width = 420;
   }
 
   skip(value) {
-    let video: any = document.getElementById("my_video_1");
+    let video: any = document.getElementById('my_video_1');
     video.currentTime += value;
   }
 
   restart() {
-    let video: any = document.getElementById("my_video_1");
+    let video: any = document.getElementById('my_video_1');
     video.currentTime = 0;
   }
 }
