caa0d88fbc9f44ebb20819e2f5d3e4e0438e0eae
Omar Mohamed <omarmohamed178@gmail.com>
2022-10-01 23:33:08 +0200

    Home UI

--- src/app/services/popup.service.ts
+++ src/app/services/popup.service.ts
@@ -2,41 +2,27 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 
-export class PopupService  {
-  constructor() { }
-  appRootUrl = environment.appRoot+'/';
-  makeCapitalPopup(data: any): string {
-    return `` +
-    
-      `
-      <a href="/project/${data.projectId}">
-      <div class="card project-card" style="width:300px" routerLink="/about" >
-      <div class="img-panel map-image-panel">
-          <img src="${data.coverImage ? this.appRootUrl+data.coverImage : 'assets/images/home-placeHolder.webp'}" class="card-img-top" alt="...">
-         
-          <div class="card-label">${data.status}</div>
-          <div class="card-mask"></div>
-      </div>
-      <div class="card-body">
-          <div class="">
-              <div class="row">
-                  <div class="col-md-12">
-                    
-                      <h6>${data.projectName}</h6>
-                      <h6>${data.city}</h6>
-                  </div>
-                  
-              </div>
-          
-              
-          </div>
-         
-      </div>
-  </div>
+export class PopupService {
+    constructor() { }
+    appRootUrl = environment.appRoot + '/';
+    makeCapitalPopup(data: any): string {
+        return `` +
+            `
+      <a href="/project/${data.projectId}" title="اعرض التفاصيل">
+        <div class="map-card" routerLink="/about" >
+            <div class="project-img">
+                <img src="${data.coverImage ? this.appRootUrl + data.coverImage : 'assets/images/home-placeHolder.webp'}" class="card-img-top" alt="...">         
+                <span class="project-status">${data.status}</span>
+            </div>
+            <div class="project-info">
+                <h6 class="project-title">${data.projectName}</h6>
+                <h6 class="project-city">${data.city}</h6>      
+            </div>
+        </div>
       </a>
       `
-  }
+    }
 }
