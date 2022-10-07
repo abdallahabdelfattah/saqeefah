cc4f81b078056d3b84410a1b9c51c934940852e5
abdallahsayed@blackstoneeit.com <abdallahsayed@blackstoneeit.com>
2022-10-08 01:02:12 +0200

    fix logo

--- src/app/components/logo-slider-one/logo-slider-one.component.ts
+++ src/app/components/logo-slider-one/logo-slider-one.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { SliderService } from 'src/app/pages/dashboard/setting/services/slider.service';
 import { changeLanguageService } from 'src/app/services/changeLanguage.service';
+import { SliderTypes } from 'src/app/shared/Enums/enums';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -10,6 +12,7 @@ import { environment } from 'src/environments/environment';
 })
 export class LogoSliderOneComponent implements OnInit {
 @Input() logos= [];
+slider:any; 
 appRootUrl=environment.appRoot+'/';
 
 customOptions: OwlOptions = {
@@ -44,18 +47,26 @@ customOptions: OwlOptions = {
 }
 bannerimgUrl= []
 
-  constructor(private language:changeLanguageService) { }
-
+  constructor(private language:changeLanguageService,private sliderServices:SliderService) { }
   ngOnInit(): void {
-    this.logos.forEach(element => {
-      this.bannerimgUrl.push(this.getUrl(this.appRootUrl+element?.path));
-    });
+    this.getSlider();
   }
 
+  getSlider(){
+    this.sliderServices.getAllSliderByidForUser(SliderTypes.OurPartners,this.language.getLanguageID()).subscribe((response:any)=>{
+  if(!response?.isError){
+      this.slider=response?.result?.data   
+    this.slider?.sliderAttachment.forEach(element => {
+      this.bannerimgUrl.push(this.getUrl(this.appRootUrl+element?.path));
+    });
+
+  }
+    })
+  }
+  
   getUrl(path: string) {
     if(path)
     {
-
       path = path.replace(/[\/\\]/g, '/');
       path = path.replace(/ /g, '%20');
       return path;
@@ -64,5 +75,7 @@ bannerimgUrl= []
       return ""; 
     }
   }
+
+
 
 }
