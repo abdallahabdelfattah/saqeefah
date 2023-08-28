d3b9f516c3b349c79277221d9df93e623320afe8
abdallah abdelfattah <abdallahabdelfattah36@gmail.com>
2023-08-28 15:35:59 +0300

    ok

--- src/app/project-villas/project-villas.component.ts
+++ src/app/project-villas/project-villas.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+import { ProjectAndListService } from '../services/project-lists.service';
+import { GenaricService } from '../services/Genaric.service';
+import { changeLanguageService } from '../services/changeLanguage.service';
 
 
 @Component({
@@ -9,7 +12,8 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 })
 export class ProjectVillasComponent implements OnInit {
 
-  constructor() { }
+  AllProjects:any ; 
+  constructor( private generalService:GenaricService, private projects:ProjectAndListService,private language:changeLanguageService) { }
 
   customOptions: OwlOptions = {
     loop: true,
@@ -39,6 +43,18 @@ export class ProjectVillasComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getAllProjects(); 
   }
+
+  getAllProjects(){
+    this.projects.getAllProjects(this.language.getLanguageID()).subscribe((response:any)=>{
+  if(!response.isError){
+    this.AllProjects = response.result.data
+    this.AllProjects=this.AllProjects.filter(x=>x['statusId']!=4);
+  }
+  
+    })
+  }
+
 
 }
